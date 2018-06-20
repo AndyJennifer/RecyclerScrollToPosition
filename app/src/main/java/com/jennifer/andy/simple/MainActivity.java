8bfe7fc7e5a0709d27f72f57205a893393d4f552
AndyJennifer <1225868370@qq.com>
2018-06-20 16:27:34 +0800

    添加了测试例子

--- app/src/main/java/com/jennifer/andy/simple/MainActivity.java
+++ app/src/main/java/com/jennifer/andy/simple/MainActivity.java
@@ -2,26 +2,65 @@ package com.jennifer.andy.simple;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private RecyclerView mRecyclerView;
+    private EditText mEditText;
+    private Button mBtnStart;
+    private List<String> mStringList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findView();
+        initData();
         initView();
     }
 
     private void findView() {
         mRecyclerView = findViewById(R.id.recycler);
+        mEditText = findViewById(R.id.et_number);
+        mBtnStart = findViewById(R.id.btn_start);
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = mEditText.getText().toString().trim();
+                if (TextUtils.isEmpty(number)) {
+                    Toast.makeText(MainActivity.this, "请输入滚动的位置", Toast.LENGTH_SHORT).show();
+                } else if (Integer.parseInt(number) > mStringList.size() - 1) {
+                    Toast.makeText(MainActivity.this, "请输入正确的范围0到100", Toast.LENGTH_SHORT).show();
+                } else {
+                    mRecyclerView.smoothScrollToPosition(Integer.parseInt(number));
+                }
+            }
+        });
     }
 
     private void initView() {
-        
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new SimpleTextAdapter(this, mStringList));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+    }
+
+    private void initData() {
+        mStringList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            mStringList.add("这是第" + i + "条数据");
+        }
+
     }
 }
---
+++ app/src/main/java/com/jennifer/andy/simple/SimpleTextAdapter.java
@@ -0,0 +1,58 @@
+package com.jennifer.andy.simple;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Author:  andy.xwt
+ * Date:    2018/6/20 15:49
+ * Description:
+ */
+
+public class SimpleTextAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<String> list;
+    private Context mContext;
+
+    public SimpleTextAdapter(Context context, List<String> list) {
+        this.mContext = context;
+        this.list = list;
+
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new SimpleTextHolder(LayoutInflater.from(mContext).inflate(R.layout.item_simple_text, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        SimpleTextHolder textHolder = (SimpleTextHolder) holder;
+        textHolder.mTextView.setText(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    private class SimpleTextHolder extends RecyclerView.ViewHolder {
+
+        TextView mTextView;
+
+        SimpleTextHolder(View itemView) {
+            super(itemView);
+            mTextView = itemView.findViewById(R.id.tv_text);
+        }
+    }
+
+
+}
